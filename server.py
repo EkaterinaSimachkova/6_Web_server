4b36b86d150683dbb6c8c2f4a71ab489adf8a681
Ekaterina <90846568+EkaterinaSimachkova@users.noreply.github.com>
2021-11-25 18:24:14 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
+import datetime
 
 sock = socket.socket()
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 try:
     sock.bind(('', 80))
@@ -9,7 +11,17 @@ except OSError:
     sock.bind(('', 8080))
     print("Using port 8080")
 
+with open("1.html", "r") as file:
+  html1 = file.read()
+
+with open("2.html", "r") as file:
+  html2 = file.read()
+
+with open("index.html", "r") as file:
+  index = file.read()
+
 sock.listen(5)
+
 
 conn, addr = sock.accept()
 print("Connected", addr)
@@ -19,13 +31,26 @@ msg = data.decode()
 
 print(msg)
 
-resp = """HTTP/1.1 200 OK
+if "1.html" in msg:
+    ChosenFile = html1
+elif "2.html" in msg:
+    ChosenFile = html2
+else:
+    ChosenFile = index
+
+now = datetime.datetime.now()
+
+
+resp = f"""HTTP/1.1 200 OK
+Date: {now.strftime("%a, %d %b %Y %H:%M:%S")}
 Server: SelfMadeServer v0.0.1
-Content-type: text/html
+Content-Length: {len(ChosenFile)}
+Content-Type: text/html
 Connection: close
 
-Hello, webworld!"""
+{ChosenFile}"""
 
+print(resp)
 conn.send(resp.encode())
 
 conn.close()
